212f95d257ab43a2aff30c660684e03a3b04fde6
Adam Garcia <adamgarcia1994@ucla.edu>
2016-05-12 02:58:47 -0700

    Hi

--- Problem4.m
+++ Problem4.m
@@ -5,7 +5,7 @@ clc;
 
 %% Parameters
 theta_total=90;
-n_el = 2;
+n_el = 10;
 n_nodes = n_el+1;
 radius = 1;
 EA_const = 1e9;
@@ -19,7 +19,7 @@ X = zeros(n_nodes,3);
 CNX = zeros(2,n_el);
 
 %% Generate EA/EI arrays
-EA = EA_const*EA;
+EA = EA_const*EA; %Make a vector here and below.
 EI = EI_const*EI;
 
 %% Generate CNX
@@ -32,6 +32,20 @@ CNX;
 
 %% Generate EQN
 
+counter = 1;
+EQN = zeros(3,n_nodes);
+for i = 1:n_nodes
+   if i ==1
+       EQN(:,1)=0;
+   else
+       EQN(1,i)=counter;
+       counter = counter+1;
+       EQN(2,i)=counter;
+       counter = counter+1;
+       EQN(3,i)=counter;
+       counter = counter+1;
+   end
+end
 
 
 %% Generate X
@@ -40,7 +54,7 @@ ThetaArray = linspace(0,theta_total*pi/180,n_nodes);
 for iter = 1:n_nodes
     X(iter, 1) = -radius*cos(ThetaArray(iter));
     X(iter, 2) = radius*sin(ThetaArray(iter));
-    X(iter, 3) = 0;
+    X(iter, 3) = pi/2-ThetaArray(iter);
 end
 X;
 
